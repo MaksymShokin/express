6c0a7baf0e18500fa113e98e4e29d98d1f2db36b
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-07 23:40:29 +0300

    applying-middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('inside middlleware');
+  next(); // allow to travel to the next middleware
+})
+
+app.use((req, res, next) => {
+  console.log('inside another middlleware')
+})
+
 const server = http.createServer(app);
 
 server.listen(3000)
