10aeb3f45a499ee0c89eb36e52981b0f1462a14b
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-09 22:57:55 +0300

    404-page

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('inside another middlleware');
   res.send('<h1>Hello from express</h1>')
 });
 
