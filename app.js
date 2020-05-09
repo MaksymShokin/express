0f5b43625868e51bf25dd6df9ddd8ee4bcffa959
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-09 14:14:13 +0300

    handling-different-routes

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('inside middlleware');
-  next();
+app.use('/users', (req, res, next) => {
+  console.log('inside users');
+  res.send('<h1>Users from express</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('inside another middlleware');
-  res.send('<h1>HEllo from express</h1>')
+  res.send('<h1>Hello from express</h1>')
 })
 
 app.listen(3000);
