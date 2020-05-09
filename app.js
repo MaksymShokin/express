a88124d94a4b16642cd169b80207f788349ad872
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-09 20:55:05 +0300

    using-body-parser

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-user', (req, res, next) => {
+  console.log('inside add user');
+  res.send('<form action="/users" method="POST"><input type="text" name="userName"/><button>Add user</button></form>')
+});
+
 app.use('/users', (req, res, next) => {
-  console.log('inside users');
-  res.send('<h1>Users from express</h1>')
-})
+  console.log(req.body);
+  res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
   console.log('inside another middlleware');
   res.send('<h1>Hello from express</h1>')
-})
+});
 
 app.listen(3000);
 
-
+//npm install --save body-parser
