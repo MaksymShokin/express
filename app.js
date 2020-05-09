50c0057c5131363660b863038ead124001e888cf
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-09 22:48:47 +0300

    express-routes

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-user', (req, res, next) => {
-  console.log('inside add user');
-  res.send('<form action="/users" method="POST"><input type="text" name="userName"/><button>Add user</button></form>')
-});
-
-//app.get
-//app.post
-
-app.post('/users', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('inside another middlleware');
-  res.send('<h1>Hello from express</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
 
-//npm install --save body-parser
