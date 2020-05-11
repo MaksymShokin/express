532978e1acfde89cfceae6271c62b3d5f1e2e503
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-11 22:55:11 +0300

    req-variable0storage

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,18 @@ const routeDir = require('../helpers/path');
 
 const router = express.Router();
 
+let products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(routeDir, 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title})
   res.redirect('/');
 });
 
-module.exports = router
+module.exports = {
+  routes: router,
+  products: products
+}
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const routeDir = require('../helpers/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log(adminData.products)
   res.sendFile(path.join(routeDir, 'views', 'shop.html'))
 });
 
