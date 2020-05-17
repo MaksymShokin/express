e18a8589234fa7e8a58bdb8fc0f5e18127a6f9b3
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-17 22:28:42 +0300

    ejs-layout

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {title: 'Page not found ejs'});
+  res.status(404).render('404', {title: 'Page not found ejs', path: ''});
 })
 
 app.listen(3000);
